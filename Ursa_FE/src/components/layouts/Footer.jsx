53cbe5c7fd371ffbf4c965e136a8e61db9dce145
Ting0824 <book1yummy2@gmail.com>
2025-04-11 09:38:13 +0800

    新增footer，路徑之後需要改

--- Ursa_FE/src/components/layouts/Footer.jsx
+++ Ursa_FE/src/components/layouts/Footer.jsx
@@ -3,7 +3,19 @@ import React, { Component } from "react";
 function Footer() {
   return (
     <>
-      <div className="bg-primary">footer</div>
+      <div className="bg-primary">
+        <div className="px-40 py-5 flex gap-x-4 justify-center text-sm">
+          <a href="/">首頁</a>
+          <a href="/">成為隅藏家</a>
+          <a href="/">常見問題</a>
+          <a href="/">關於我們</a>
+          <a href="/">聯絡我們</a>
+        </div>
+        <hr />
+        <p className="px-40 py-5 flex justify-center text-sm">
+          Copyright © 2025 Personal Liberary. All Rights Reserved.{" "}
+        </p>
+      </div>
     </>
   );
 }
